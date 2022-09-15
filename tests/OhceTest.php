e89140802eedaa659bbfadaeb7e5e827d66eb184
MKoding <mikel.romano@540deg.com>
2022-09-15 12:22:38 +0200

    Inicio de Kata

---
+++ tests/OhceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class OhceTest extends TestCase
+{
+    /**
+    * @test
+    */
+    public function test()
+    {
+        $this->assertTrue(True);
+    }
+}
